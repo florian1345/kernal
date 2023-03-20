2e70d7bb34483697c64fb7f589a7f9a8213407d4
florian1345 <florian1345@gmx.de>
2023-03-20 20:50:05 +0100

    Add assertions for Maps with PartialEq values

--- src/util.rs
+++ src/util.rs
@@ -3,3 +3,11 @@ use std::borrow::Borrow;
 pub(crate) fn borrow_all<T, B: Borrow<T>>(to_borrow: &[B]) -> Vec<&T> {
     to_borrow.iter().map(|b| b.borrow()).collect()
 }
+
+pub(crate) fn borrow_all_pairs<L, R, LB, RB>(to_borrow: &[(LB, RB)]) -> Vec<(&L, &R)>
+where
+    LB: Borrow<L>,
+    RB: Borrow<R>
+{
+    to_borrow.iter().map(|(l, r)| (l.borrow(), r.borrow())).collect()
+}
