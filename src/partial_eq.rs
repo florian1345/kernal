7093043537007fe411733c98f40a3c92d3f7dbb2
florian1345 <florian1345@gmx.de>
2023-05-27 23:07:44 +0200

    Add is_equal_to_any and is_equal_to_none

--- src/partial_eq.rs
+++ src/partial_eq.rs
@@ -5,6 +5,8 @@ use std::borrow::Borrow;
 use std::fmt::Debug;
 
 use crate::{AssertThat, Failure};
+use crate::collections::{CollectionDebug, HighlightedCollectionDebug};
+use crate::util::borrow_all;
 
 /// An extension trait to be used on the output of [assert_that](crate::assert_that) with an
 /// argument that implements the [PartialEq] trait.
@@ -14,7 +16,7 @@ use crate::{AssertThat, Failure};
 /// ```
 /// use kernal::prelude::*;
 ///
-/// assert_that!(1 + 1).is_equal_to(2).is_not_equal_to(3);
+/// assert_that!(1 + 1).is_equal_to(2).is_equal_to_none([1, 3]);
 /// ```
 pub trait PartialEqAssertions<T> {
 
@@ -25,6 +27,14 @@ pub trait PartialEqAssertions<T> {
     /// Asserts that the tested value is different from the given `expected` value according to the
     /// [PartialEq] trait.
     fn is_not_equal_to<E: Borrow<T>>(self, expected: E) -> Self;
+
+    /// Asserts that the tested value is equal to one element from the given `expected_iter`
+    /// according to the [PartialEq] trait.
+    fn is_equal_to_any<E: Borrow<T>, I: IntoIterator<Item = E>>(self, expected_iter: I) -> Self;
+
+    /// Asserts that the tested value is not equal to any element from the given `unexpected_iter`
+    /// according to the [PartialEq] trait.
+    fn is_equal_to_none<E: Borrow<T>, I: IntoIterator<Item = E>>(self, unexpected_iter: I) -> Self;
 }
 
 impl<T: Debug + PartialEq> PartialEqAssertions<T> for AssertThat<T> {
@@ -48,6 +58,47 @@ impl<T: Debug + PartialEq> PartialEqAssertions<T> for AssertThat<T> {
             Failure::new(&self)
                 .expected_it(format!("not to equal <{:?}>", expected_data))
                 .but_it("did")
+                .fail();
+        }
+
+        self
+    }
+
+    fn is_equal_to_any<E: Borrow<T>, I: IntoIterator<Item = E>>(self, expected_iter: I) -> Self {
+        let expected_vec_unborrowed = expected_iter.into_iter().collect::<Vec<_>>();
+        let expected_vec: Vec<&T> = borrow_all(&expected_vec_unborrowed);
+
+        if expected_vec.iter().any(|&expected| &self.data == expected) {
+            return self;
+        }
+
+        let collection_debug = CollectionDebug {
+            collection: &expected_vec
+        };
+
+        Failure::new(&self)
+            .expected_it(format!("to equal any of <{:?}>", collection_debug))
+            .but_it_was_data(&self)
+            .fail()
+    }
+
+    fn is_equal_to_none<E: Borrow<T>, I: IntoIterator<Item = E>>(self, unexpected_iter: I) -> Self {
+        let unexpected_vec_unborrowed = unexpected_iter.into_iter().collect::<Vec<_>>();
+        let unexpected_vec: Vec<&T> = borrow_all(&unexpected_vec_unborrowed);
+
+        let counter_example_index = unexpected_vec.iter().enumerate()
+            .find(|(_, &unexpected)| &self.data == unexpected)
+            .map(|(index, _)| index);
+
+        if let Some(counter_example_index) = counter_example_index {
+            let collection_debug = HighlightedCollectionDebug {
+                collection: &unexpected_vec,
+                highlighted_sections: vec![counter_example_index..(counter_example_index + 1)]
+            };
+
+            Failure::new(&self)
+                .expected_it(format!("not to equal any of <{:?}>", collection_debug))
+                .but_it_was_data(&self)
                 .fail();
         }
 
@@ -114,5 +165,53 @@ mod tests {
         assert_fails!((21 + 21).is_not_equal_to(U32Wrapper { data: 42 }),
             expected it "not to equal <42>"
             but it "did");
+    }
+
+    #[test]
+    fn is_equal_to_any_passes_for_singleton_list_containing_tested_element() {
+        assert_that!(42).is_equal_to_any([42]);
+    }
+
+    #[test]
+    fn is_equal_to_any_passes_for_list_containing_tested_element_later() {
+        assert_that!(36).is_equal_to_any([35, 36, 37]);
+    }
+
+    #[test]
+    fn is_equal_to_any_fails_for_empty_list() {
+        assert_fails!((42).is_equal_to_any([] as [i32; 0]),
+            expected it "to equal any of <[ ]>"
+            but it "was <42>");
+    }
+
+    #[test]
+    fn is_equal_to_any_fails_for_larger_list_not_containing_tested_element() {
+        assert_fails!((36).is_equal_to_any([12, 13, 35, 37, 42]),
+            expected it "to equal any of <[ 12, 13, 35, 37, 42 ]>"
+            but it "was <36>");
+    }
+
+    #[test]
+    fn is_equal_to_none_passes_for_empty_list() {
+        assert_that!(42).is_equal_to_none([] as [i32; 0]);
+    }
+
+    #[test]
+    fn is_equal_to_none_passes_for_larger_list_not_containing_tested_element() {
+        assert_that!(36).is_equal_to_none([12, 13, 35, 37, 42]);
+    }
+
+    #[test]
+    fn is_equal_to_none_fails_for_singleton_list_containing_tested_element() {
+        assert_fails!((42).is_equal_to_none([42]),
+            expected it "not to equal any of <[ [42] ]>"
+            but it "was <42>");
+    }
+
+    #[test]
+    fn is_equal_to_none_fails_for_list_containing_tested_element_later() {
+        assert_fails!((36).is_equal_to_none([35, 36, 37]),
+            expected it "not to equal any of <[ 35, [36], 37 ]>"
+            but it "was <36>");
     }
 }
